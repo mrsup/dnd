7da3362cc1bad2f3bed4eaab090af6595e5ddbdf
Xiaolong Huang <xiaolong@chinaz.edu.tf>
2015-07-16 15:07:10 +0800

    Create hxl.js
    黄小龙个人部分的js

---
+++ js/hxl.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function rec(){
+    var mychar="QQ:525901794";
+alert(mychar)
+  }
+function hidepic(){
+document.getElementById("con").style.display="none";
+}
+function showpic(){
+ document.getElementById("con").style.display="block";
+}
