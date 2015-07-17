a12510e55949946723a15b72180e6821f528aeb4
sup <mrsup@sina.com>
2015-07-17 13:01:43 +0800

    deal with the bug.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-	$('#dowebok').fullpage({
-		sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90','#8A8890', '#4BBFC3', '#7BAABE', '#f90'],
-		'navigation': true,
-	});
+$('#dowebok').fullpage({
+	sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90','#8A8890', '#4BBFC3', '#7BAABE', '#f90'],
+	'navigation': true,
 });
